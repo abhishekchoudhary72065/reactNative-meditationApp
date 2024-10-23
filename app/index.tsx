d43301fb64b5de07d51dc99ca2941dfa94ab1624
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-10-23 19:34:31 +0530

    initial commit with tailwind setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center bg-blue-500 items-center">
+      <Text className="font-bold text-2xl">This is the index.tsx file</Text>
+    </View>
+  );
+}
