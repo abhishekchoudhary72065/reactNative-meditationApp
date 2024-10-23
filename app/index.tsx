21f90e68ee5b28de2c880387b5b86f4f6350450a
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-10-23 20:19:53 +0530

    added custom button component & fix for image imports in image.d.ts file

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,60 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  SafeAreaView,
+  Platform,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
 import "../global.css";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { LinearGradient } from "expo-linear-gradient";
+import CustomButton from "@/components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center bg-blue-500 items-center">
-      <Text className="font-bold text-2xl">This is the index.tsx file</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <SafeAreaView
+            className="gap-10 justify-between flex-1 mx-5 my-10"
+            style={styles.androidSafeView}
+          >
+            <View>
+              <Text className="font-bold text-white text-4xl text-center">
+                Simple Medidation
+              </Text>
+              <Text className="text-white text-2xl text-center mt-3">
+                Simplifying Medidation
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                onPress={() => console.log("hello")}
+                title="Get Started!!"
+              />
+            </View>
+            <StatusBar barStyle={"light-content"} />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  androidSafeView: {
+    paddingTop:
+      Platform.OS === "android" && StatusBar?.currentHeight
+        ? StatusBar?.currentHeight
+        : 0,
+  },
+});
